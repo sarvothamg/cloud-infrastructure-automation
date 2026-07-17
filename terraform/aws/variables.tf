ba0ec4adba7bac9554222b0d69923d7f44cc15b6
Sarvotham <sarvotham90@gmail.com>
2026-07-16 18:48:39 -0700

    Create variables.tf

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for infrastructure deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
